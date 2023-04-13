41e5b353dc80c468a27870613910c804b1f8e389
LWalia17 <121966833+LWalia17@users.noreply.github.com>
2023-04-13 11:16:26 +0530

    Update and rename createTables.psql to createTables.sql

---
+++ sql/ddl/createTables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS public.user (
+  userid INT  NOT NULL,
+  name VARCHAR(120) NOT NULL,
+  ip VARCHAR(45) NOT  NULL,
+  PRIMARY KEY (userid))
+  
+CREATE TABLE IF NOT EXISTS public.activity (
+  category  VARCHAR(120) NOT NULL,
+  label  VARCHAR(120) NULL,
+  action  VARCHAR(120)  NULL,
+  search  VARCHAR(120)  NOT NULL,
+  timestamp  VARCHAR(120) NOT NULL,
+  userid INT NULL)
+
+
+CREATE TABLE IF NOT EXISTS public.user_login (
+  ip  VARCHAR(120) NOT NULL,
+  browser  VARCHAR(120) NULL,
+  userid INT  NOT NULL
+)
